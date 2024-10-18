c9cd1aacbd07cb8faf4b13fb8e384c3eab4213f5
Sjdenhartog <sam.denhartog@live.nl>
2024-10-18 12:28:31 +0200

    Added all the simple kLa relations

--- Strip_alcohol.py
+++ Strip_alcohol.py
@@ -42,7 +42,8 @@ V_G = (eps_G*V_L)/(1-eps_G)
 # Use Empirical relation: Deckwer1, Deckwer2, Jackson_Shen, Heijnen_VanRiet
 k_H = Henry(T_K)/101325 # mol/(m3*Pa)
 
-kLa = Empirical.Deckwer1(u_G) # s-1
+
+#kLa = Empirical.Deckwer1(u_G) # s-1
 
 # Constants
 R = 8.314 # J/(mol*K)
@@ -57,9 +58,7 @@ def alcohol_conc(t, var):
 
     C_alc = var[0]
     gam = var[1]
-    #C_gas = 1/k_H * C_alc
-    #r = -kLa * (C_alc-(P_vap*101325/(R*T_K)))#mol/m3/s
-    #r = -kLa * (C_alc-(P_vap/k_H))#mol/m3/s
+
     CL_star = k_H * gam * P_tot
     r = kLa*(CL_star-C_alc)
     F[0] = r # mol/(m3*s) --> dC_L/dt
@@ -71,21 +70,45 @@ def alcohol_conc(t, var):
 total_time = 8 * 3600 # s
 
 
-print(kLa*3600)
-gam_0 = 0
-sol = solve_ivp(alcohol_conc, (0, total_time), y0=[C_begin, gam_0], method='LSODA')
-
 fig, ax1 = plt.subplots()
-
-color='tab:red'
-ax1.plot(sol.t/3600, sol.y[0]*MW_eth/1000, color=color)
-ax1.set_ylabel('Concentration (g/L)', color=color)
+kLa_relations = [Empirical.Deckwer1, Empirical.Deckwer2, Empirical.Jackson_Shen, Empirical.Heijnen_VantRiet]
+relation_names = ['Deckwer1', 'Deckwer2', 'Jackson and Shen', "Heijnen Van 't Riet"]
+kLa_all = []
+for name, relation in enumerate(kLa_relations):
+    if relation == Empirical.Heijnen_VantRiet:
+        kLa = relation(u_G, T_K)
+        kLa_all.append(kLa)
+    else:
+        kLa = relation(u_G)
+        kLa_all.append(kLa)
+    
+    gam_0 = 0
+    sol = solve_ivp(alcohol_conc, (0, total_time), y0=[C_begin, gam_0], method='LSODA')
+    
+    ax1.plot(sol.t/3600, sol.y[0]*MW_eth/1000, label=relation_names[name]+r'$k_La$' + ' = ' + f'{round(kLa*3600, 0)}' + r'$h^{-1}$')
+ax1.set_ylabel('Concentration (g/L)')
 ax1.set_xlabel('Time (h)')
-ax1.tick_params(axis='y', labelcolor=color)
+plt.legend(loc='upper right')
+  
 
-color='tab:blue'
+
+# print(kLa*3600)
+# gam_0 = 0
+# sol = solve_ivp(alcohol_conc, (0, total_time), y0=[C_begin, gam_0], method='LSODA')
+
+# fig, ax1 = plt.subplots()
+
+# color='tab:red'
+# ax1.plot(sol.t/3600, sol.y[0]*MW_eth/1000, color=color)
+# ax1.set_ylabel('Concentration (g/L)', color=color)
+# ax1.set_xlabel('Time (h)')
+# ax1.tick_params(axis='y', labelcolor=color)
+
+color='tab:purple'
 ax2 = ax1.twinx()
 ax2.plot(sol.t/3600, sol.y[1], color='tab:blue')
 ax2.set_ylabel('Fraction of ethanol in offgas', color=color)
 ax2.tick_params(axis='y', labelcolor=color)
+
 plt.show()
+
